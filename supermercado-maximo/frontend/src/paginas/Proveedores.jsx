8412d86fb367c336e4f8436734a521d0c39fe69e
carlosGB0518 <carlosgonzalez6789@hotmail.com>
2025-11-11 02:02:33 +0000

    funcionamiento temporal

--- supermercado-maximo/frontend/src/paginas/Proveedores.jsx
+++ supermercado-maximo/frontend/src/paginas/Proveedores.jsx
@@ -1,9 +1,4 @@
-import { useEffect, useState } from 'react';
-import { supabase } from '../supabase';
-import LayoutBase from '../layouts/LayoutBase';
-import jsPDF from 'jspdf';
-import autoTable from 'jspdf-autotable';
-import '../estilos/proveedores.css';
+
 
 const Proveedores = () => {
   return <div style={{ padding: '2rem' }}>✅ Página de proveedores cargada correctamente</div>;
