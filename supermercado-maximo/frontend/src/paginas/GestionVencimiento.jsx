4dfef0e6eacd5105873bb71e1897027f3934ba86
carlosGB0518 <carlosgonzalez6789@hotmail.com>
2026-05-09 02:50:37 +0000

    .

--- supermercado-maximo/frontend/src/paginas/GestionVencimiento.jsx
+++ supermercado-maximo/frontend/src/paginas/GestionVencimiento.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { supabase } from '../supabase';
 import LayoutBase from '../layouts/LayoutBase';
-import ModalEditarVencimiento from '../paginas/ModalEditarVencimiento';
+import ModalEditarVencimiento from './ModalEditarVencimiento';
 import '../estilos/GestionVencimientos.css';
 
 function GestionVencimientos() {
